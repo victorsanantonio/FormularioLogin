9bc6f5119417f9308b98376b68a69252f87c98c1
victorsanantonio <victorsanantonio@hotmail.com>
2021-05-06 10:45:00 +0200

    Proyecto creado

---
+++ src/main/java/com/example/demo/FormularioLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioLoginApplication.class, args);
+	}
+
+}
